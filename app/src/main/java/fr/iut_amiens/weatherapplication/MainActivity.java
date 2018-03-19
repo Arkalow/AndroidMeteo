3e9b488f9f7bd4fd284b43f5e3b25992927e3f8c
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-03-19 15:31:32 +0100

    Affichage des valeurs des champs

--- app/src/main/java/fr/iut_amiens/weatherapplication/MainActivity.java
+++ app/src/main/java/fr/iut_amiens/weatherapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package fr.iut_amiens.weatherapplication;
 
 import android.os.Bundle;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 
@@ -14,7 +18,14 @@ import fr.iut_amiens.weatherapplication.openweathermap.WeatherResponse;
 public class MainActivity extends AppCompatActivity implements WeatherListener{
 
     private WeatherManager weatherManager;
-    private TextView textView;
+    private TextView temps;
+    private TextView temps_description;
+    private TextView temperature;
+    private TextView pressure;
+    private TextView humidity;
+    private TextView speed;
+    private TextView lastUpdate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +51,40 @@ public class MainActivity extends AppCompatActivity implements WeatherListener{
         WeatherTask weatherTask = new WeatherTask();
         weatherTask.addListener(this);
         weatherTask.execute();
-        textView = findViewById(R.id.);
+
+        temps = findViewById(R.id.temps);
+        temps_description = findViewById(R.id.tempsDescription);
+        temperature = findViewById(R.id.temperature_value);
+        pressure = findViewById(R.id.pressure_value);
+        humidity = findViewById(R.id.humidity_value);
+        speed = findViewById(R.id.speed_value);
+        lastUpdate = findViewById(R.id.lastUpdate_value);
+
     }
 
     @Override
-    public void getWeather(WeatherResponse weather) {
-        Log.d("getWeather", weather.getName());
-        textView.setText(weather.toString());
-        this.setTitle(weather.getName());
+    public void getWeather(WeatherResponse weatherResponse) {
+        WeatherResponse.Weather weather = weatherResponse.getWeather().get(0);
+        Log.d("getWeather", weatherResponse.getName());
+        this.setTitle(weatherResponse.getName());
+
+        setText(temps, weather.getMain(), "None");
+        setText(temps_description, weather.getDescription(), "None");
+        setText(temperature, weatherResponse.getMain().getTemp() + " C°", "None");
+        setText(pressure,  weatherResponse.getMain().getPressure() + " hPa", "None");
+        setText(humidity, weatherResponse.getMain().getHumidity() + "%", "None");
+        setText(speed, weatherResponse.getWind().getSpeed() + " m/s", "None");
+        setText(lastUpdate, "Comming soon", "None");
+    }
+
+    public void setText(TextView champs, String value, String defaultValue) {
+        try {
+            champs.setText(value);
+        }catch (NullPointerException nullPointerException ){
+            Log.e("Champs", nullPointerException.getMessage());
+            champs.setText(defaultValue);
+        }catch (Exception exception){
+            Log.e("Champs", exception.getMessage());
+        }
     }
 }

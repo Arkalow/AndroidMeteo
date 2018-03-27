3994510c6771fe14b6604cd86c80085a9bdf51f1
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-03-28 01:15:12 +0200

    Affichage des bonnes valeurs dans le recyclerView

--- app/src/main/java/fr/iut_amiens/weatherapplication/MeteoViewHolder.java
+++ app/src/main/java/fr/iut_amiens/weatherapplication/MeteoViewHolder.java
@@ -3,7 +3,10 @@ package fr.iut_amiens.weatherapplication;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import fr.iut_amiens.weatherapplication.openweathermap.ForecastResponse;
 
@@ -12,12 +15,22 @@ import fr.iut_amiens.weatherapplication.openweathermap.ForecastResponse;
  */
 public class MeteoViewHolder extends RecyclerView.ViewHolder {
 
-    private TextView textView;
     private ForecastResponse.Forecast forcast;
+
+    private TextView date;
+    private TextView temps;
+    private TextView temps_description;
+    private TextView temperature;
+    private ImageView imageView;
 
     public MeteoViewHolder(View itemView) {
         super(itemView);
-        textView = itemView.findViewById(R.id.item);
+
+        date = itemView.findViewById(R.id.date);
+        temps = itemView.findViewById(R.id.temps);
+        temps_description = itemView.findViewById(R.id.tempsDescription);
+        temperature = itemView.findViewById(R.id.temperature);
+        imageView = itemView.findViewById(R.id.imageView);
     }
 
     /***
@@ -26,11 +39,28 @@ public class MeteoViewHolder extends RecyclerView.ViewHolder {
      */
     public void bind(ForecastResponse.Forecast forecast) {
         this.forcast = forecast;
-        textView.setText(forecast.toString());
-        Log.d("MeteoViewHolder", textView.getText().toString());
+        ForecastResponse.Weather weather = forecast.getWeather().get(0);
+
+        setText(date, forecast.getDatetime().toString());
+        setText(temps, weather.getMain());
+        setText(temps_description, weather.getDescription());
+        setText(temperature, forecast.getMain().getTemp() + " C°");
+
+        //Chargement de l'image
+        Picasso.with(itemView.getContext()).load(weather.getIcon()).into(imageView);
+        Log.d("MeteoViewHolder", forecast.getWeather().toString());
     }
 
-    public void recycle() {
-        textView.setText("");
+    /***
+     * Set value on textView and check null value
+     * @param champs
+     * @param value
+     */
+    public void setText(TextView champs, String value) {
+        try {
+            champs.setText(value);
+        } catch (Exception exception) {
+            Log.e("Champs", exception.getMessage());
+        }
     }
 }
